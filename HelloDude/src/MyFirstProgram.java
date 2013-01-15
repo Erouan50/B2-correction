35c3c00b70d3c767040b58175dda257ffc34101b
Antoine Rouaze <antoine.rouaze@zenika.com>
2013-01-15 13:02:16 +0100

    Add HelloDude Tp correction.

---
+++ HelloDude/src/MyFirstProgram.java
@@ -0,0 +1,9 @@
+/**
+ * @author Antoine Rouaze <antoine.rouaze@zenika.com>
+ */
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Dude !!!");
+    }
+}
