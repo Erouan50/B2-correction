a4aa9e463633ce95d9707d1b1e13907a59db6a50
Antoine Rouaze <antoine.rouaze@zenika.com>
2013-01-17 14:30:17 +0100

    Update GameOfLife Tp correction.

--- GameOfLife/src/com/supinfo/gameoflife/Launcher.java
+++ GameOfLife/src/com/supinfo/gameoflife/Launcher.java
@@ -6,13 +6,13 @@ package com.supinfo.gameoflife;
 public class Launcher {
 
     public static void main(String[] args) {
-        World world = new World(10, 10);
+        World world = new World(100, 100);
         System.out.println(world);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000000; i++) {
             world.newGeneration();
             System.out.println(world);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
