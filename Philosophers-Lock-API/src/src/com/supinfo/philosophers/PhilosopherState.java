948055131dad3bc0597cea7cd35738fd21a895e4
Antoine Rouaze <antoine.rouaze@zenika.com>
2013-04-04 15:39:52 +0200

    Add Philosophers Tp correction.

---
+++ Philosophers-Lock-API/src/src/com/supinfo/philosophers/PhilosopherState.java
@@ -0,0 +1,9 @@
+package src.com.supinfo.philosophers;
+
+/**
+ * @author Antoine Rouaze <antoine.rouaze@zenika.com>
+ */
+public enum PhilosopherState {
+
+    HUNGRY, EATING, THINKING
+}
---
+++ Philosophers/src/com/supinfo/philosophers/PhilosopherState.java
@@ -0,0 +1,9 @@
+package com.supinfo.philosophers;
+
+/**
+ * @author Antoine Rouaze <antoine.rouaze@zenika.com>
+ */
+public enum PhilosopherState {
+
+    HUNGRY, EATING, THINKING
+}
